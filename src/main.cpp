918f0244c59e95cd820a67139839fe518638bb84
Simone Gorini <simone.gorini@ageval.fr>
2021-01-12 10:52:30 +0100

    just started, no biggie

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
-#include <iostream>
 #include <SDL2/SDL.h>
 
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 
+
 int main(int argc, char *argv[])
 {
-	std::cout << "Hello, your makefile works\n";
 	return 0;
 }
